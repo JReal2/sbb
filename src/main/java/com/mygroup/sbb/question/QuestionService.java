3f799f8d7ee6a9f70cf1a5d72d26e43ebb706be4
JRLee <49389212+JReal2@users.noreply.github.com>
2023-07-31 11:55:42 +0900

    Apply Pagination

--- src/main/java/com/mygroup/sbb/question/QuestionService.java
+++ src/main/java/com/mygroup/sbb/question/QuestionService.java
@@ -1,10 +1,16 @@
 package com.mygroup.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mygroup.sbb.DataNotFoundException;
-import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -15,12 +21,15 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
-        return this.questionRepository.findById(id).orElseThrow(()->new DataNotFoundException("존재하지 않는 질문입니다." + id));
+        return this.questionRepository.findById(id).orElseThrow(() -> new DataNotFoundException("존재하지 않는 질문입니다." + id));
     }
 
     public void create(String subject, String content) {
@@ -30,4 +39,5 @@ public class QuestionService {
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
     }
+
 }
