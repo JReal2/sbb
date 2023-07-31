f2c5989bf5ed6731d9612b1031bc6d90e916e68e
JRLee <49389212+JReal2@users.noreply.github.com>
2023-07-31 11:55:42 +0900

    Apply Pagination

--- src/test/java/com/mygroup/sbb/SbbApplicationTests.java
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -1,37 +1,23 @@
 package com.mygroup.sbb;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.mygroup.sbb.answer.Answer;
-import com.mygroup.sbb.question.Question;
-import com.mygroup.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.mygroup.sbb.question.QuestionService;
 
 @SpringBootTest
-@Transactional
-@Commit
 class SbbApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 }
