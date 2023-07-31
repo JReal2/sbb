c7cb68e9176ae3fc52ff0ac9fb31c71ce52ea61e
JRLee <49389212+JReal2@users.noreply.github.com>
2023-07-31 15:50:44 +0900

    add user entity on question and answer

--- src/main/java/com/mygroup/sbb/SecurityConfig.java
+++ src/main/java/com/mygroup/sbb/SecurityConfig.java
@@ -11,39 +11,41 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-                .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
-                .headers((headers) -> headers
-                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-                .formLogin((formLogin) -> formLogin
-                        .loginPage("/user/login")
-                        .defaultSuccessUrl("/"))
-                .logout((logout) -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                        .logoutSuccessUrl("/")
-                    .invalidateHttpSession(true));
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                                                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                                .csrf((csrf) -> csrf
+                                                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                                .headers((headers) -> headers
+                                                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                                .formLogin((formLogin) -> formLogin
+                                                .loginPage("/user/login")
+                                                .defaultSuccessUrl("/"))
+                                .logout((logout) -> logout
+                                                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                                                .logoutSuccessUrl("/")
+                                                .invalidateHttpSession(true));
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-            
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        @Bean
+        AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+                        throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
 }
--- src/main/java/com/mygroup/sbb/answer/AnswerController.java
+++ src/main/java/com/mygroup/sbb/answer/AnswerController.java
@@ -2,13 +2,19 @@ package com.mygroup.sbb.answer;
 
 import com.mygroup.sbb.question.Question;
 import com.mygroup.sbb.question.QuestionService;
+import com.mygroup.sbb.user.UserService;
+
 import lombok.RequiredArgsConstructor;
+
+import java.security.Principal;
+import com.mygroup.sbb.user.SiteUser;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
@@ -20,16 +26,19 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm,
-            BindingResult bindingResult) {
+            BindingResult bindingResult, Principal principal) {
         Question question = this.questionService.getQuestion(id);
+        SiteUser author = this.userService.getUser(principal.getName());
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent());
+        this.answerService.create(question, answerForm.getContent(), author);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
