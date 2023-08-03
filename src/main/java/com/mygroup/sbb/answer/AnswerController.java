cc9739b1451ff9d0ea6a279fea0515d3d3072c60
JRLee <49389212+JReal2@users.noreply.github.com>
2023-08-03 12:44:16 +0900

    Add vote function

--- src/main/java/com/mygroup/sbb/answer/AnswerController.java
+++ src/main/java/com/mygroup/sbb/answer/AnswerController.java
@@ -9,12 +9,15 @@ import lombok.RequiredArgsConstructor;
 import java.security.Principal;
 import com.mygroup.sbb.user.SiteUser;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
@@ -40,5 +43,50 @@ public class AnswerController {
         }
         this.answerService.create(question, answerForm.getContent(), author);
         return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+
+        answerForm.setContent(answer.getContent());
+        return String.format("question_detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("modify/{id}")
+    public String answerModify(AnswerForm answerForm, BindingResult bindingResult,
+            Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Answer answer = this.answerService.getAnswer(id);
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String answerVote(Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = this.answerService.getAnswer(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.answerService.vote(answer, siteUser);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("delete/{id}")
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+
+        this.answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 }
--- src/main/java/com/mygroup/sbb/answer/AnswerService.java
+++ src/main/java/com/mygroup/sbb/answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.mygroup.sbb.answer;
 
+import com.mygroup.sbb.DataNotFoundException;
 import com.mygroup.sbb.question.Question;
 import com.mygroup.sbb.user.SiteUser;
 
@@ -21,5 +22,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        return this.answerRepository.findById(id).orElseThrow(() -> new DataNotFoundException("존재하지 않는 답변입니다." + id));
+    }
+
+    public void vote(Answer answer, SiteUser siteUser) {
+        answer.getVoter().add(siteUser);
+        this.answerRepository.save(answer);
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
